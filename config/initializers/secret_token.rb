33b4b74bc514fd0791ba2095ff7d0eeabe386f53
Miguel Gonzalez <maggonzz@gmail.com>
2013-02-08 18:38:43 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fmk::Application.config.secret_token = 'b5f78c30e31ede7cb487e3a8b7e2c0b164d903c349e23fb48434771e6279b5f4cbc8c96c234728741f571f5935796af65cf9f798572242aac45b45e0940ab7ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fmk::Application.config.session_store :cookie_store, key: '_fmk_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fmk::Application.config.session_store :active_record_store
